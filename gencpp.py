663cf94b52c5e8b25a4c674bc85beae577bea369
ocfbnj <ocfbnj@qq.com>
2022-04-10 19:21:13 +0800

    update cmake-conan to 0.18.1

--- gencpp.py
+++ gencpp.py
@@ -109,9 +109,8 @@ list(APPEND CMAKE_PREFIX_PATH ${{CMAKE_BINARY_DIR}})
 
 if(NOT EXISTS "${{CMAKE_BINARY_DIR}}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/release/0.17/conan.cmake"
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
          "${{CMAKE_BINARY_DIR}}/conan.cmake"
-         EXPECTED_HASH SHA256=3bef79da16c2e031dc429e1dac87a08b9226418b300ce004cc125a82687baeef
          TLS_VERIFY ON)
 endif()
 
